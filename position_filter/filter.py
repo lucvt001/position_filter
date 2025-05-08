f8b2b1d7b0f996354f9206f0a53a36f864323eed
Luc <luc001@e.ntu.edu.sg>
2025-05-08 13:19:15 +0000

    Big cleanup

--- position_filter/filter.py
+++ position_filter/filter.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
-from geometry_msgs.msg import Point, Twist, TransformStamped
+from std_msgs.msg import Float32, Float32MultiArray
+from geometry_msgs.msg import TransformStamped
 import numpy as np
 from filterpy.kalman import UnscentedKalmanFilter, MerweScaledSigmaPoints
 from filterpy.common import Q_discrete_white_noise
@@ -20,25 +20,38 @@ class PositionFilter(Node):
         self.horizontal_thrust_vector = 0.
         self.dt = 0.1
 
+        # Parameters
+        leader2_offset = self.declare_parameter("leader2_offset", -10.0).get_parameter_value().double_value
+        leader1_distance_topic = self.declare_parameter("leader1_distance_topic", "distance_to_leader1").get_parameter_value().string_value
+        leader2_distance_topic = self.declare_parameter("leader2_distance_topic", "distance_to_leader2").get_parameter_value().string_value
+        state_topic = self.declare_parameter("state_topic", "ukf/state").get_parameter_value().string_value
+        covariance_topic = self.declare_parameter("covariance_topic", "ukf/covariance").get_parameter_value().string_value
+        self.parent_frame = self.declare_parameter("parent_frame", "leader1/base_link").get_parameter_value().string_value
+        self.child_frame = self.declare_parameter("child_frame", "follower/ukf_link").get_parameter_value().string_value
+        self.Q_std = self.declare_parameter("Q_std", 0.9).get_parameter_value().double_value
+        self.R_std = self.declare_parameter("R_std", 0.3).get_parameter_value().double_value
+        self.initial_estimate = self.declare_parameter("initial_estimate", [-10.0, 0.0, -2.0, 0.0]).get_parameter_value().double_array_value
+
         # Offsets
         self.leader1_offset = np.array([0., 0.])
-        self.leader2_offset = np.array([0., -10.])
+        self.leader2_offset = np.array([0., leader2_offset])
 
         # Subscribers containing the range of the follower from the leaders
-        self.leader1_distance_sub = self.create_subscription(Float32, "/follower/distance_to_leader1", self.leader1_distance_cb, 1)
-        self.leader2_distance_sub = self.create_subscription(Float32, "/follower/distance_to_leader2", self.leader2_distance_cb, 1)
+        self.leader1_distance_sub = self.create_subscription(Float32, leader1_distance_topic, partial(self.distance_cb, offset=self.leader1_offset), 1)
+        self.leader2_distance_sub = self.create_subscription(Float32, leader2_distance_topic, partial(self.distance_cb, offset=self.leader2_offset), 1)
 
         # Publisher to publish the filtered data of the follower
-        self.filtered_position_pub = self.create_publisher(Point, '/follower/filtered_position', 1)
-        self.filtered_velocity_pub = self.create_publisher(Twist, '/follower/filtered_velocity', 1)
+        self.state_pub = self.create_publisher(Float32MultiArray, state_topic, 1)
+        self.covariance_pub = self.create_publisher(Float32MultiArray, covariance_topic, 1)
 
         # Transform broadcaster for dynamic transform publishing
         self.tf_broadcaster = TransformBroadcaster(self)
 
-        self.parent_frame = self.declare_parameter("parent_frame", "supreme_leader")
-        self.child_frame = self.declare_parameter("child_frame", "follower/filtered_position")
-        # self.parent_frame = self.get_parameter("parent_frame").get_parameter_value().string_value
-        # self.child_frame = self.get_parameter("child_frame").get_parameter_value().string_value
+        ns = self.get_namespace()
+        if ns == "/":
+            ns = "follower"     # Default namespace if none is provided
+        if self.child_frame.startswith("NS"):
+            self.child_frame = self.child_frame.replace("NS", ns)
 
         # Timer for predict step of the filter
         self.timer = self.create_timer(self.dt, self.filter_predict)
@@ -57,13 +70,18 @@ class PositionFilter(Node):
     def filter_reset(self):
         points = MerweScaledSigmaPoints(n=4, alpha=1e-2, beta=2., kappa=-1.0)
         self.ukf = UnscentedKalmanFilter(dim_x=4, dim_z=1, dt=self.dt, points=points, fx=self.state_transition_function, hx=None)    
-        self.ukf.x = np.array([-10., 0.0, -2.0, 0.0])
+        self.ukf.x = np.array(self.initial_estimate)  # Initial state estimate
         self.ukf.P = np.diag([9., 9., 9., 9.])
-        self.ukf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.9**2, block_size=2)  # Process noise. Choose dt=1 because dt=0.1 cause the Q matrix to be too small, leading to numerical issues
-        self.ukf.R = np.array([0.3**2]) # Measurement noise
+        self.ukf.Q = Q_discrete_white_noise(dim=2, dt=self.dt, var=self.Q_std**2, block_size=2)
+        self.ukf.R = np.array([self.R_std**2]) # Measurement noise
+        self.get_logger().info(f'UKF initialized with state: {self.ukf.x.tolist()}')
 
     def filter_predict(self):
+        P = self.ukf.P.diagonal()
+        if P[0] > 20 and P[2] > 20:  # Stop predicting if the covariance is too large because it means there has been no update for a while
+            return
         self.ukf.predict()
+        self.publish_state_and_covariance()       
         # self.get_logger().info(f'UKF state: {self.ukf.x.tolist()}')
 
     def state_transition_function(self, x: np.ndarray, dt: float):
@@ -72,56 +90,37 @@ class PositionFilter(Node):
         pos_y += vel_y * dt
         return np.array([pos_x, vel_x, pos_y, vel_y])
 
-    def leader1_distance_cb(self, msg: Float32):
+    def distance_cb(self, msg: Float32, offset: np.ndarray):
         time_now = self.get_clock().now()
         time_elapsed = (time_now - self.prev_update_time).nanoseconds / 1e9
         if time_elapsed <= 0.11:    # to avoid updating the filter many times at once which can cause it to collapse
             return 
-        distance_to_leader1 = msg.data
-        self.update_and_publish(np.array([distance_to_leader1]), self.leader1_offset)
         self.prev_update_time = time_now
+        
+        measurement = msg.data
+        self.ukf.hx = partial(self.measurement_function, offset=offset)
+        self.ukf.update(z=measurement)
 
-        # Log diagonal values of P covariance matrix
-        p = self.ukf.P.tolist()
-        self.get_logger().info(f'P covariance matrix: {p[0][0]}, {p[1][1]}, {p[2][2]}, {p[3][3]}')
-
-    def leader2_distance_cb(self, msg: Float32):
-        time_now = self.get_clock().now()
-        time_elapsed = (time_now - self.prev_update_time).nanoseconds / 1e9
-        if time_elapsed <= 0.11:
-            return       
-        distance_to_leader2 = msg.data
-        self.update_and_publish(np.array([distance_to_leader2]), self.leader2_offset)     
-        self.prev_update_time = time_now
-
-        # Log diagonal values of P covariance matrix
-        p = self.ukf.P.tolist()
-        self.get_logger().info(f'P covariance matrix: {p[0][0]}, {p[1][1]}, {p[2][2]}, {p[3][3]}')
+        self.smoothen_state_and_broadcast_transform()
+        self.publish_state_and_covariance()
 
     def measurement_function(self, x: np.ndarray, offset: np.ndarray):
         pos_x, _, pos_y, _ = x
         return np.sqrt([ (pos_x-offset[0]) ** 2 + (pos_y-offset[1]) ** 2 ])
-    
-    def update_and_publish(self, measurement: np.ndarray, offset: np.ndarray):
-        self.ukf.hx = partial(self.measurement_function, offset=offset)
-        self.ukf.update(z=measurement)
-        self.publish_data()
 
-    def publish_data(self):
-        pos_x, vel_x, pos_y, vel_y = self.ukf.x
+    def publish_state_and_covariance(self):
+        state = Float32MultiArray()
+        state.data = self.ukf.x.tolist()
+        self.state_pub.publish(state)
+
+        covariance = Float32MultiArray()
+        covariance.data = self.ukf.P.flatten().tolist()
+        self.covariance_pub.publish(covariance)
+
+    def smoothen_state_and_broadcast_transform(self):
+        pos_x, _, pos_y, _ = self.ukf.x
         pos_x = self.wma(pos_x, self.rolling_pos_x, self.wma_weights)
         pos_y = self.wma(pos_y, self.rolling_pos_y, self.wma_weights)
-        point = Point()
-        point.x = pos_x
-        point.y = pos_y
-        point.z = 0.0  # Assuming z is 0 for this example
-        self.filtered_position_pub.publish(point)
-        # self.get_logger().info(f'Publishing Point: x={point.x}, y={point.y}, z={point.z}')
-
-        twist = Twist()
-        twist.linear.x = vel_x
-        twist.linear.y = vel_y
-        self.filtered_velocity_pub.publish(twist)
 
         # Publish dynamic transform
         transform = TransformStamped()
